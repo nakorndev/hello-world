fe64a9967e70c53851978482493d4657456e0bf6
nakorndev <nakorndev@gmail.com>
2021-01-05 15:25:48 +0700

    Initial project

---
+++ web/script.js
@@ -0,0 +1,43 @@
+Vue.createApp({
+  data () {
+    return {
+      isLoading: true,
+      profiles: [],
+      search: ''
+    }
+  },
+  computed: {
+    searchProfiles () {
+      if (!this.search) return this.profiles
+      const founds = []
+      for (const profile of this.profiles) {
+        if (profile.full_name.includes(this.search)) {
+          founds.push(profile)
+        }
+      }
+      return founds
+    },
+    devMode () {
+      return location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+    }
+  },
+  async mounted () {
+    const jsonUrl = this.devMode
+      ? '../docs/profiles.json'
+      : 'https://raw.githubusercontent.com/nakorncode/hello-world/gh-pages/profiles.json'
+    try {
+      const { data } = await axios.get(jsonUrl)
+      this.profiles = shuffle(data)
+    } catch (error) {
+      console.error(error)
+    }
+    this.isLoading = false
+  },
+  methods: {
+    avatarUrl (profile) {
+      return this.devMode
+        ? `../docs/avatars/${profile.file_name}.svg`
+        : `./avatars/${profile.file_name}.svg`
+    }
+  }
+}).mount('#app')
